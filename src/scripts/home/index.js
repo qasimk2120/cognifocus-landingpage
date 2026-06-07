fd1a9f844881dc4b5e44d7c4089022d0a938bb42
Mohammed Qasim <78582000+qasimk2120@users.noreply.github.com>
2026-06-07 08:18:33 +0300

    feat: add Goblin ambient background layer with animations and integrate into layout

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,5 +1,4 @@
 import { initHomeFaqAccordion } from "./faq-accordion.js";
-import { initBehaviorCompanion } from "./behavior-companion.js";
 import { initAttentionSpiralsCarousel } from "./attention-spirals-carousel.js";
 import { initGoblinBehaviorCards } from "./goblin-cards.js";
 import { initReactionCarousel } from "./reaction-carousel.js";
@@ -13,5 +12,4 @@ export function initHomePage() {
   initAttentionSpiralsCarousel();
   initGoblinBehaviorCards();
   initHomeFaqAccordion();
-  initBehaviorCompanion();
 }
